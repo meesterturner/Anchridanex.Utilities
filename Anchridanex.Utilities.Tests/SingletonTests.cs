165c6b4f99f09e047b530c07a7a49002e95d8112
Paul Turner <paul.turner611@gmail.com>
2024-12-28 15:05:44 +0000

    Added Singleton abstract class to the utilities collection

---
+++ Anchridanex.Utilities.Tests/SingletonTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anchridanex.Utilities.Tests
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void TestDirectInstancing()
+        {
+            Exception x = new AccessViolationException();
+            Assert.ThrowsException<AccessViolationException>(() => { TestSingletonInt newInstance = new(); });
+        }
+
+        [TestMethod]
+        public void TestDuplicateInstancing()
+        {
+            TestSingletonInt.Instance.Value = 123;
+            TestSingletonInt.Instance.Value = 789;
+            Assert.ThrowsException<InvalidOperationException>(() => { TestSingletonInt newInstance = new(); });
+        }
+
+        [TestMethod]
+        public void TestSingletonInstancing()
+        {
+            Random rng = new();
+            int randomValue = rng.Next(0, 100);
+
+            TestSingletonInt.Instance.Value = randomValue;
+
+            Assert.IsTrue(TestSingletonInt.Instance.Value == randomValue);
+        }
+
+        [TestMethod]
+        public void TestMultipleDistinctSingletons()
+        {
+            const int INT_VALUE = 1979;
+            const string STRING_VALUE = "Words";
+
+            TestSingletonInt.Instance.Value = INT_VALUE;
+            TestSingletonString.Instance.Value = STRING_VALUE;
+
+            Assert.IsTrue(TestSingletonInt.Instance.Value == INT_VALUE &&
+                TestSingletonString.Instance.Value == STRING_VALUE);
+        }
+
+
+    }
+
+    public class TestSingletonInt : Singleton<TestSingletonInt>
+    {
+        public int Value { get; set; }
+    }
+
+    public class TestSingletonString : Singleton<TestSingletonString>
+    {
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ Anchridanex.Utilities/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anchridanex.Utilities
+{
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T? _instance = default(T);
+        private static bool _instancing = false;
+
+        public Singleton()
+        {
+            if (_instance != null)
+                throw new InvalidOperationException($"Singleton {nameof(T)} already instanced");
+
+            if (_instancing == false)
+                throw new AccessViolationException($"Singleton {nameof(T)} being instance directly");
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instancing = true;
+                    _instance = new T();
+                    _instancing = false;
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
