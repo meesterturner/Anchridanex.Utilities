247531e11f13485d855d4019fe13d20b78dab739
Paul Turner <paul.turner611@gmail.com>
2025-02-20 20:57:47 +0000

    Added LruCache class

---
+++ Anchridanex.Utilities.Tests/LruCacheTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anchridanex.Utilities.Tests
+{
+    [TestClass]
+    public class LruCacheTests
+    {
+        [TestMethod]
+        public void TestCache()
+        {
+            int values = 100;
+            int matches = 0;
+
+            LruCache<int, int> cache = new(values);
+            for (int i = 1; i <= 100; i++)
+            {
+                cache[i] = Answer(i);
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int fromCache = cache[i];
+                int expected = Answer(i);
+                if (fromCache != expected)
+                    Assert.Fail($"Value {i} returned {fromCache} instead of {expected}");
+                else
+                    matches++;
+            }
+
+            Assert.AreEqual(values, matches);
+        }
+
+        private int Answer(int i)
+        {
+            return i * 79;
+        }
+
+    }
+}
---
+++ Anchridanex.Utilities/LruCache.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anchridanex.Utilities
+{
+    public class LruCache<TKey, TValue> where TKey : notnull
+    {
+        private const int DEFAULT_CAPACITY = 100;
+        private int _capacity;
+        private Queue<TKey> keyQueue;
+        private Dictionary<TKey, TValue?> cacheDict;
+
+        public LruCache()
+        {
+            _capacity = DEFAULT_CAPACITY;
+            keyQueue = new(_capacity);
+            cacheDict = new(_capacity);
+        }
+
+        public LruCache(int capacity)
+        {
+            _capacity = capacity;
+            keyQueue = new(_capacity);
+            cacheDict = new(_capacity);
+        }
+
+        public TValue? this[TKey key]
+        {
+            get
+            {
+                TValue? value;
+                bool success = TryGetValue(key, out value);
+
+                if (success)
+                    return value;
+
+                return default(TValue);
+            }
+
+            set
+            {
+                Add(key, value);
+            }
+        }
+
+        public void Add(TKey key, TValue? value)
+        {
+            if (keyQueue.Count == _capacity)
+            {
+                TKey oldest = keyQueue.Dequeue();
+                cacheDict.Remove(oldest);
+            }
+
+            cacheDict[key] = value;
+            keyQueue.Enqueue(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue? value)
+        {
+            return cacheDict.TryGetValue(key, out value);
+        }
+    }
+}
